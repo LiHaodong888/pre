68c609783b83ad43845ac96a5f589f82087ffc46
lihaodong <lihaodongmail@163.com>
2019-05-13 11:07:01 +0800

    “message”

--- src/main/java/com/xd/pre/log/SysLogAspect.java
+++ src/main/java/com/xd/pre/log/SysLogAspect.java
@@ -25,7 +25,7 @@ import java.util.Objects;
 /**
  * @Classname SysLogAspect
  * @Description 系统日志切面
- * @Author 李号东 lihaodongmail@163.com
+ * @Author 李号东 lihaodongmail@163.comgit reset --merge
  * @Date 2019-04-22 23:52
  * @Version 1.0
  * ①切面注解得到请求数据 -> ②发布监听事件 -> ③异步监听日志入库
