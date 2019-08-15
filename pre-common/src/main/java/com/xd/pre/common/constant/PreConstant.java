b2239a5b8be822196d6cbd8722571526df5e14eb
lihaodong <lihaodongmail@163.com>
2019-08-15 09:17:54 +0800

    修复第三方登录依赖Bug

--- pre-common/src/main/java/com/xd/pre/common/constant/PreConstant.java
+++ pre-common/src/main/java/com/xd/pre/common/constant/PreConstant.java
@@ -11,6 +11,7 @@ public class PreConstant {
 
     public static final String RESET_MAIL = "MAIL";
     public static final String PRE_IMAGE_KEY = "PRE:IMAGE:KEY:";
+    public static final String PRE_TENANT_KEY = "TENANT_ID";
 
 
 }
